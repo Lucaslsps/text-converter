c540b0e1a38d2a0f1488d99f2242dad382c706a5
Lucas Peixoto <lucas.lsps@hotmail.com>
2020-07-02 19:53:24 -0300

    Sem bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useImperativeHandle } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Form, Button } from "react-bootstrap";
 
 const App = () => {
   const [selected, setSelected] = useState();
@@ -12,64 +11,61 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <Form>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>
-            Enter your text and choose from one of the options
-          </Form.Label>
-          <Form.Control as="textarea" rows="3"></Form.Control>
-        </Form.Group>
-        <Form.Check
-          inline
-          label="Sentence case"
+    <div className="wrapper">
+      <h1>TEXT Converter</h1>
+
+      <input
+        type="textarea"
+        placeholder="Enter your text and choose from one of the options"
+      />
+
+      <form>
+        <label htmlFor="1">Sentence case</label>
+        <input
+          type="radio"
           id="1"
           name="textMode"
           onClick={handleCheck}
+          placeholder="asfasf"
         />
-        <Form.Check
-          inline
-          label="lower case"
-          id="2"
-          name="textMode"
-          onClick={handleCheck}
-        />
-        <Form.Check
-          inline
-          label="UPPER CASE"
-          id="3"
-          name="textMode"
-          onClick={handleCheck}
-        />
-        <Form.Check
-          inline
+        <label htmlFor="2">lower case</label>
+        <input type="radio" id="2" name="textMode" onClick={handleCheck} />
+        <label htmlFor="3">UPPER CASE</label>
+        <input type="radio" id="3" name="textMode" onClick={handleCheck} />
+        <label htmlFor="4">Capitalized Case</label>{" "}
+        <input
+          type="radio"
           label="Capitalized Case"
           id="4"
           name="textMode"
           onClick={handleCheck}
         />
-        <Form.Check
-          inline
+        <label htmlFor="5">aLtErNaTiNg cAsE</label>
+        <input
+          type="radio"
           label="aLtErNaTiNg cAsE"
           id="5"
           name="textMode"
           onClick={handleCheck}
         />
-        <Form.Check
-          inline
+        <label htmlFor="6">Title Case</label>
+        <input
+          type="radio"
           label="Title Case"
           id="6"
           name="textMode"
           onClick={handleCheck}
         />
-        <Form.Check
+        <label htmlFor="7">InVeRsE CaSe</label>
+        <input
+          type="radio"
           inline
           label="InVeRsE CaSe"
           id="7"
           name="textMode"
           onClick={handleCheck}
         />
-      </Form>
+      </form>
     </div>
   );
 };
